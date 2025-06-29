c4962920f600762b946a148c0991f787d992dccf
Mborini <mborini98@gmail.com>
2025-06-29 17:49:34 +0300

    param route

--- src/app/api/products-by-brand/[id]/route.ts
+++ src/app/api/products-by-brand/[id]/route.ts
@@ -3,10 +3,10 @@ import { NextRequest } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  context: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = context.params;
+    const { id } = await params;
 
     const client = await pool.connect();
 
