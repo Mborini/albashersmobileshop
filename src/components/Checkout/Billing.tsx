6191db0a53c812e0e7680ef285dfc7b0f2ac6723
mbirni <mborini98@gmail.com>
2025-07-18 18:57:34 +0300

    checkout style

--- src/components/Checkout/Billing.tsx
+++ src/components/Checkout/Billing.tsx
@@ -1,138 +1,111 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import {
+  TextInput,
+  Textarea,
+  Checkbox,
+  Box,
+  Group,
+  Title,
+  Paper,
+} from "@mantine/core";
 
 const Billing = () => {
   const { t, i18n } = useTranslation();
+  const [email, setEmail] = useState("");
+  const [confirmSendEmail, setConfirmSendEmail] = useState(false);
 
   return (
     <div dir={i18n.language === "ar" ? "rtl" : "ltr"}>
-      <div className="bg-black text-white shadow-1 rounded-[10px] p-4 mb-6">
-        <h2 className="font-medium  text-xl sm:text-2xl">
+      <Paper bg="black" c="white" shadow="md" radius="lg" p="md" mb="md">
+        <Title order={2} size="h4">
           {t("billing.title")}
-        </h2>
-      </div>
+        </Title>
+      </Paper>
 
-      <div className="bg-white shadow-1 rounded-[10px] p-4 sm:p-8.5">
-        <div className="flex flex-col lg:flex-row gap-5 sm:gap-8 mb-5">
-          <div className="w-full">
-            <label htmlFor="firstName" className="block mb-2.5">
-              {t("billing.firstName")} <span className="text-red">*</span>
-            </label>
-            <input
-              type="text"
-              name="firstName"
-              id="firstName"
-              placeholder={t("billing.firstName")}
-              required
-              className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-            />
-          </div>
-
-          <div className="w-full">
-            <label htmlFor="lastName" className="block mb-2.5">
-              {t("billing.lastName")} <span className="text-red">*</span>
-            </label>
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              placeholder={t("billing.lastName")}
-              required
-              className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-col lg:flex-row gap-5 sm:gap-8 mb-5">
-          <div className="w-full mb-5">
-            <label htmlFor="email" className="block mb-2.5">
-              {t("billing.email")} <span className="text-red">*</span>
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder={t("billing.email")}
-              required
-              className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-            />
-          </div>
-
-          <div className="w-full mb-5">
-            <label htmlFor="phone" className="block mb-2.5">
-              {t("billing.phone")} <span className="text-red">*</span>
-            </label>
-            <input
-              type="text"
-              name="phone"
-              id="phone"
-              placeholder={t("billing.phone")}
-              required
-              className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-col lg:flex-row gap-5 sm:gap-8 mb-5">
-          <div className="w-full mb-5">
-            <label htmlFor="country" className="block mb-2.5">
-              {t("billing.country")} <span className="text-red">*</span>
-            </label>
-            <input
-              type="text"
-              name="country"
-              id="country"
-              placeholder={t("billing.country")}
-              required
-              className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-            />
-          </div>
-
-          <div className="w-full mb-5">
-            <label htmlFor="city" className="block mb-2.5">
-              {t("billing.city")} <span className="text-red">*</span>
-            </label>
-            <input
-              type="text"
-              name="city"
-              id="city"
-              placeholder={t("billing.city")}
-              required
-              className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-            />
-          </div>
-        </div>
-
-        <div className="mb-5">
-          <label htmlFor="address" className="block mb-2.5">
-            {t("billing.address")} <span className="text-red">*</span>
-          </label>
-          <input
-            type="text"
-            name="address"
-            id="address"
-            placeholder={t("billing.addressPlaceholder")}
+      <Paper shadow="md" radius="lg" p="md" mb="xl" withBorder>
+        <Group grow align="flex-start" mb="md">
+          <TextInput
+            variant="filled"
+            radius="lg"
             required
-            className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
+            placeholder={t("billing.firstName")}
           />
-        </div>
+          <TextInput
+            variant="filled"
+            radius="lg"
+            required
+            placeholder={t("billing.lastName")}
+          />
+        </Group>
 
-        <div className="mb-5.5">
-          <div>
-            <label htmlFor="note" className="block mb-2.5">
-              {t("billing.notes")}
-            </label>
-            <textarea
-              name="note"
-              id="note"
-              rows={5}
-              placeholder={t("billing.notesPlaceholder")}
-              className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full p-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
-            ></textarea>
-          </div>
-        </div>
-      </div>
+        <Group grow align="flex-start" mb="md">
+          <Box w="100%">
+            <TextInput
+              variant="filled"
+              radius="lg"
+              type="email"
+              placeholder={t("billing.email")}
+              value={email}
+              onChange={(e) => {
+                setEmail(e.currentTarget.value);
+                if (!e.currentTarget.value) setConfirmSendEmail(false);
+              }}
+            />
+            {email && (
+              <Checkbox
+                radius="lg"
+                
+                color="green"
+                size="xs"
+                label={t("billing.confirmSend")}
+                checked={confirmSendEmail}
+                onChange={(e) => setConfirmSendEmail(e.currentTarget.checked)}
+                mt="sm"
+                style={{ color: "green" }}
+              />
+            )}
+          </Box>
+
+          <TextInput
+            variant="filled"
+            radius="lg"
+            required
+            placeholder={t("billing.phone")}
+          />
+        </Group>
+
+        <Group grow align="flex-start" mb="md">
+          <TextInput
+            variant="filled"
+            radius="lg"
+            required
+            placeholder={t("billing.country")}
+          />
+          <TextInput
+            variant="filled"
+            radius="lg"
+            required
+            placeholder={t("billing.city")}
+          />
+        </Group>
+
+        <TextInput
+          variant="filled"
+          radius="lg"
+          required
+          placeholder={t("billing.addressPlaceholder")}
+          mb="md"
+        />
+
+        <Textarea
+          variant="filled"
+          radius="lg"
+          placeholder={t("billing.notesPlaceholder")}
+          minRows={4}
+        />
+      </Paper>
     </div>
   );
 };
