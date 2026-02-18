2702140abff3c54f823a9900196caa2d9afbb3db
mbirni <mborini98@gmail.com>
2026-02-19 02:41:02 +0300

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,13 @@
 // middleware.ts
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-
-  // فقط عندما يدخل المستخدم على المسار /
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL('/en', request.url));
-  }
 
   return NextResponse.next();
 }
 
 // شغّل الميدل وير على المسار الجذر فقط
 export const config = {
-  matcher: ['/'],
+  matcher: ["/"],
 };
